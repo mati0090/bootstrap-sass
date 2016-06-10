d3598a13144715c9c003d89e79598b6ec52c3fad
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-06-10 08:40:16 +0100

    rake convert

--- assets/javascripts/bootstrap/tooltip.js
+++ assets/javascripts/bootstrap/tooltip.js
@@ -319,9 +319,11 @@
 
     function complete() {
       if (that.hoverState != 'in') $tip.detach()
-      that.$element
-        .removeAttr('aria-describedby')
-        .trigger('hidden.bs.' + that.type)
+      if (that.$element) { // TODO: Check whether guarding this code with this `if` is really necessary.
+        that.$element
+          .removeAttr('aria-describedby')
+          .trigger('hidden.bs.' + that.type)
+      }
       callback && callback()
     }
 
@@ -480,6 +482,7 @@
       that.$tip = null
       that.$arrow = null
       that.$viewport = null
+      that.$element = null
     })
   }
 
